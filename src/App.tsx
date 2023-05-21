f2c9e2162f8c716137ca14c89dcd84a2fe7273bd
D3FAU4T <d3fau4t@gmail.com>
2023-05-21 09:35:52 +0530

    Made a loop to show off animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Play from './Pages/Play';
 import Editor from './Pages/Editor';
+import Scoreboard from './Pages/Scoreboard';
 
 export default function App() {
+  const [page, setPage] = useState("Play");
+
   return (
     <main>
-      <Play />
-      {/* <Editor /> */}
+      {
+        page === "Play" ? <Play PageChanger={setPage} /> :
+        page === "Editor" ? <Editor /> :
+        page === "Scoreboard" ? <Scoreboard PageChanger={setPage} /> : null
+      }      
     </main>
   )
 }
